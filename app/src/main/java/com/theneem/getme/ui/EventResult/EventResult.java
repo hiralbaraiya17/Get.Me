6500848cfa52914c18eb5843cb1df52d048f893b
hiral baraiya <hiral.baraiya17@gmail.com>
2020-04-08 13:01:26 -0400

    Initial commit - blank prototype screens

---
+++ app/src/main/java/com/theneem/getme/ui/EventResult/EventResult.java
@@ -0,0 +1,41 @@
+package com.theneem.getme.ui.EventResult;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.theneem.getme.R;
+
+
+
+
+public class EventResult extends Fragment {
+
+    private EventResultViewModel mViewModel;
+
+    public static EventResult newInstance() {
+        return new EventResult();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.event_result_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(EventResultViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/theneem/getme/ui/EventResult/EventResultViewModel.java
@@ -0,0 +1,7 @@
+package com.theneem.getme.ui.EventResult;
+
+import androidx.lifecycle.ViewModel;
+
+public class EventResultViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
