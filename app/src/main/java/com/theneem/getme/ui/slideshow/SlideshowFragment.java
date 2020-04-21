2a2c30ec07d85e7977b3d2b5d61a5b97ef0a45c7
Deval Bhavsar <theneem@gmail.com>
2020-04-20 23:47:11 -0400

    Event List completed fetching data from  my sql table and display in listview.

--- app/src/main/java/com/theneem/getme/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/theneem/getme/ui/slideshow/SlideshowFragment.java
@@ -2,7 +2,9 @@ package com.theneem.getme.ui.slideshow;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.media.Image;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -31,12 +33,37 @@ import com.theneem.getme.R;
 import com.theneem.getme.ui.EventDetailsFragment;
 import com.theneem.getme.ui.EventDetailsViewModel;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import com.squareup.picasso.Picasso;
+
+
+
+
 public class SlideshowFragment extends Fragment {
     private SlideshowViewModel slideshowViewModel;
-    public String[] arrayeventList = {"EVENT 1", "EVENT 2", "EVENT 3", "EVENT 4"};
-    private int[] IMAGES = {R.drawable.img1, R.drawable.img2, R.drawable.img3, R.drawable.img4};
+    public String[] arrayeventList; //  = {"EVENT 1", "EVENT 2", "EVENT 3", "EVENT 4"};
+    public String[] arrayeventDescList;
+    public String[] arrayeventStartDateList;
+    public String[] arrayeventEndDateList;
+
+
+    private String[] IMAGES; //  = {R.drawable.img1, R.drawable.img2, R.drawable.img3, R.drawable.img4};
     private FragmentActivity myContext;
-//    @Override
+
+    EditText txtSearch;
+    ListView listView;
+
+
+    //    @Override
 //    public void onAttach(Activity activity) {
 //        myContext=(FragmentActivity) activity;
 //        super.onAttach(activity);
@@ -48,37 +75,22 @@ public class SlideshowFragment extends Fragment {
                 ViewModelProviders.of(this).get(SlideshowViewModel.class);
         final View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
         //Next Button
-        final Button btnNext = root.findViewById(R.id.btn_next);
+        //final Button btnNext = root.findViewById(R.id.btn_next);
 
-        EditText txtSearch = (EditText) root.findViewById(R.id.txtSearch);
+         txtSearch = (EditText) root.findViewById(R.id.txtSearch);
         EventActivity eventActivity = (EventActivity) getActivity();
         txtSearch.setText(eventActivity.getMyData());
 
 
-        ListView listView = root.findViewById(R.id.lv_event_list);
-        CustomAdapter customAdapter = new CustomAdapter();
-        listView.setAdapter(customAdapter);
+        getJSON("http://scienceclub.in/getmeapi/getevents.php");
 
-        //Fragment Manager
-//
-//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                String name = arrayeventList[position].toString();
-//               FragmentTransaction fragmentTransaction1 = getFragmentManager().beginTransaction();
-//                fragmentTransaction1.replace(R.id.fragment_container_view_tag,new EventDetailsFragment());
-//                fragmentTransaction1.commit();
-                //FragmentTransaction fragmentTransaction = myContext.getSupportFragmentManager().beginTransaction();
 
-                btnNext.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
+         listView = root.findViewById(R.id.lv_event_list);
+       // CustomAdapter customAdapter = new CustomAdapter();
+        //listView.setAdapter(customAdapter);
 
-                    }
-                });
 
-//            }
-//        });
+
         return root;
     }
     class CustomAdapter extends BaseAdapter{
@@ -102,12 +114,156 @@ public class SlideshowFragment extends Fragment {
         public View getView(int position, View convertView, ViewGroup parent) {
             convertView = getLayoutInflater().inflate(R.layout.raw_layout,null);
             ImageView imageView = convertView.findViewById(R.id.raw_img);
-            TextView textView = convertView.findViewById(R.id.raw_tv);
-            imageView.setImageResource(IMAGES[position]);
-            textView.setText(arrayeventList[position]);
+
+            TextView tvEventName = convertView.findViewById(R.id.txtEventName);
+            tvEventName.setText(arrayeventList[position]);
+
+
+            TextView tvEventDesc = convertView.findViewById(R.id.txtEventDesc);
+            tvEventDesc.setText(arrayeventDescList[position]);
+
+            TextView tvStartDate = convertView.findViewById(R.id.txtStartDate);
+            tvStartDate.setText(arrayeventStartDateList[position]);
+
+            TextView tvEndDate = convertView.findViewById(R.id.txtEndDate);
+            tvEndDate.setText(arrayeventEndDateList[position]);
+
+
+            Picasso.with(getContext() ).load(IMAGES[position]).into(imageView);
 
             return convertView;
         }
     }
+
+
+
+
+    // get data from php service
+
+
+    //this method is actually fetching the json string
+    private void getJSON(final String urlWebService) {
+        /*
+         * As fetching the json string is a network operation
+         * And we cannot perform a network operation in main thread
+         * so we need an AsyncTask
+         * The constrains defined here are
+         * Void -> We are not passing anything
+         * Void -> Nothing at progress update as well
+         * String -> After completion it should return a string and it will be the json string
+         * */
+        class GetJSON extends AsyncTask<Void, Void, String> {
+
+            //this method will be called before execution
+            //you can display a progress bar or something
+            //so that user can understand that he should wait
+            //as network operation may take some time
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            //this method will be called after execution
+            //so here we are displaying a toast with the json string
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                //Toast.makeText(getActivity(), s, Toast.LENGTH_SHORT).show();
+                //txtSearch.setText(s);
+                try {
+                    loadIntoListView(s);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            //in this method we are fetching the json string
+            @Override
+            protected String doInBackground(Void... voids) {
+
+
+
+                try {
+                    //creating a URL
+                    URL url = new URL(urlWebService);
+
+                    //Opening the URL using HttpURLConnection
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+                    //StringBuilder object to read the string from the service
+                    StringBuilder sb = new StringBuilder();
+
+                    //We will use a buffered reader to read the string from service
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    //A simple string to read values from each line
+                    String json;
+
+                    //reading until we don't find null
+                    while ((json = bufferedReader.readLine()) != null) {
+
+                        //appending it to string builder
+                        sb.append(json + "\n");
+                    }
+
+                    //finally returning the read string
+                    return sb.toString().trim();
+                } catch (Exception e) {
+                    return null;
+                }
+
+            }
+        }
+
+        //creating asynctask object and executing it
+        GetJSON getJSON = new GetJSON();
+        getJSON.execute();
+    }
+
+
+
+    private void loadIntoListView(String json) throws JSONException {
+        //creating a json array from the json string
+        JSONArray jsonArray = new JSONArray(json);
+
+        //creating a string array for listview
+        String[] heroes = new String[jsonArray.length()];
+
+        arrayeventList = new String[jsonArray.length()];
+        arrayeventDescList = new String[jsonArray.length()];
+        arrayeventStartDateList = new String[jsonArray.length()];
+        arrayeventEndDateList = new String[jsonArray.length()];
+        IMAGES = new String[jsonArray.length()];
+
+        //looping through all the elements in json array
+        for (int i = 0; i < jsonArray.length(); i++) {
+
+            //getting json object from the json array
+            JSONObject obj = jsonArray.getJSONObject(i);
+
+            //getting the name from the json object and putting it inside string array
+            heroes[i] = obj.getString("event_name");
+            arrayeventList[i] = obj.getString("event_name");
+            arrayeventDescList[i] = obj.getString("event_desc");
+            arrayeventStartDateList[i] = obj.getString("event_startdate");
+            arrayeventEndDateList[i] = obj.getString("event_enddate");
+            IMAGES[i] =  obj.getString("events_img");
+
+
+        }
+
+
+        CustomAdapter customAdapter = new CustomAdapter();
+        listView.setAdapter(customAdapter);
+
+
+        //the array adapter to load data into list
+        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, heroes);
+
+        //attaching adapter to listview
+        //listView.setAdapter(arrayAdapter);
+    }
+
+
 
 }
